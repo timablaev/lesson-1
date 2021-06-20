f78a5a1aa9ba9e58274cf5d7810fcc08785face5
Tim <timablaev@ukr.net>
2021-06-20 20:46:00 +0300

    homework for lesson 2

--- src/nix_solutions/Main.java
+++ src/nix_solutions/Main.java
@@ -3,6 +3,26 @@ package nix_solutions;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello World");
+        int a = 347693485;
+        int i;
+        int x;
+        for ( i = 0; i < 9; i++) {
+            x =a % 10;
+            a = a / 10;
+            if (x % 2 == 0 && x % 3 == 0){
+                System.out.println("fizzbuzz");
+            }
+            else  if (x % 2 == 0){
+                System.out.println("fizz");
+            }
+            else if (x % 3 == 0){
+                System.out.println("buzz");
+            }
+
+            else{
+                System.out.println(x);
+            }
+        }
+
     }
 }
